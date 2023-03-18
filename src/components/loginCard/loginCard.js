6b65f78323430cd3fad7cf6f7c77305bf58d9d5f
Guilherme <glmribeiroc@gmail.com>
2023-03-18 03:09:03 -0300

    Centralizando o login form

--- src/components/loginCard/loginCard.js
+++ src/components/loginCard/loginCard.js
@@ -1,10 +1,9 @@
 import styles from './loginCard.module.css'
 
-export default function LoginCard({title, children}){
+export default function LoginCard({children}){
     return(
-        <div className={styles.card}>
-            <h2 className={styles.title}>{title}</h2>
+        <form className={styles.card}>
             {children}
-        </div>
+        </form>
     )
 }
